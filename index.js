15a6eec1dc441d1e13b33ff6851a05f2cbe78f0b
Krii41 <134600504+Krii41@users.noreply.github.com>
2025-10-20 20:28:46 +0200

    Products added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const signInBtn = document.getElementById("signInBtn");
+/* const signInBtn = document.getElementById("signInBtn");
 const modal = document.getElementById("modal");        
 const closeBtn = document.getElementById("btn-close");     
 const loginForm = document.getElementById("login-form");    
@@ -27,3 +27,48 @@ const loginForm = document.getElementById("login-form");
     const name = (localStorage.getItem("username")).trim();
     profileName.textContent = name;
   }
+*/
+
+
+const productsContainer = document.querySelector("#products-container");
+const API_URL = "https://v2.api.noroff.dev/rainy-days"; 
+
+
+async function createProducts() {
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        const products = data.data;
+
+        products.forEach(product => {
+            const card = document.createElement("div");
+            const image = document.createElement("img");
+            const title = document.createElement("h3");
+            const price = document.createElement("p");
+            const anchor = document.createElement("a");
+
+            card.className = "product-card";
+            image.className = "pc-img";
+            title.className = "pc-title";
+            price.className = "pc-price";
+
+            image.src = product.image.url;
+            image.alt = product.image.alt;
+            title.textContent = product.title;
+            price.textContent = product.price;
+            anchor.href = `product/index.html?id=${product.id}`;
+
+            card.appendChild(image);
+            card.appendChild(title);
+            card.appendChild(price);
+            anchor.appendChild(card);
+
+            productsContainer.appendChild(anchor);
+
+        })
+    } catch (error) {
+        console.error("Failed to create products", err); 
+    }
+}
+
+createProducts();
